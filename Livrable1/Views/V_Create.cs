f22ec51f3281cc505ca8618a5391bafc2c5d9582
Hugo CHAMPY <hugo.champy@viacesi.fr>
2022-11-26 20:45:10 +0100

    vue create

---
+++ Livrable1/Views/V_Create.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Resources;
+
+public class V_Create
+{
+	private viewModel oViewModel;
+    
+	public V_Create(viewModel VM)
+	{
+        //TODO language
+        this.oViewModel = VM;
+        //TODO check if there is less than 5 save jobs
+        Console.Clear();
+        Console.WriteLine("job ?");
+        string job = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("source ?");
+        string source = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine("dest ?");
+        string dest = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine(@"type ?
+            1 ful
+            2 differential");
+        string type = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine(@$"job: {job}
+source:{source}
+dest:{dest}
+type:{type}");
+    }
+}
