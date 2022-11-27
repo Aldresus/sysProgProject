7a927fce7f878f50a2ebfcd5fc444e0cffbd9ba0
Hugo CHAMPY <hugo.champy@viacesi.fr>
2022-11-27 13:33:28 +0100

    methodes lecture console & vérif

---
+++ Livrable1/Utils/U_Reader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace NSUtils
+{
+    public class U_Reader
+    {
+
+        public static int ReadInt(string promptText)
+        {
+            //TODO translate error messages
+
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                Console.WriteLine(promptText);
+                string userInput = Console.ReadLine();
+
+                if (userInput.Length > 0)
+                {
+                    try
+                    {
+                        int output = int.Parse(userInput);
+                        validInput = true;
+                        return output;
+                    }
+                    catch (System.FormatException e)
+                    {
+                        Console.WriteLine("NaN");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("cannot be empty");
+                }
+            }
+            return -1; // should never happen
+        }
+        public static string ReadString(string promptText, bool canBeEmpty)
+        {
+            //TODO translate error messages
+
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                Console.WriteLine(promptText);
+                string userInput = Console.ReadLine();
+
+                if (userInput.Length == 0 && canBeEmpty)
+                {
+                    validInput = true;
+                    return userInput;
+                }
+                else if (userInput.Length > 0)
+                {
+                    validInput = true;
+                    return userInput;
+                }
+                else
+                {
+                    Console.WriteLine("cannot be empty");
+                }
+            }
+            return "errored"; // should never happen
+        }
+    }
+}
