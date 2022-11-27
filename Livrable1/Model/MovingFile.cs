78483fe3ea1f8a2b303599e995a4b37372dfadb0
Hugo CHAMPY <hugo.champy@viacesi.fr>
2022-11-27 13:31:56 +0100

    ajout namespace

--- Livrable1/Model/MovingFile.cs
+++ Livrable1/Model/MovingFile.cs
@@ -1,61 +1,65 @@
 // Class MovingFile
 // Description: This class is used to store information about the file that will move.
 
+using System;
 using System.ComponentModel.DataAnnotations;
 
-public class M_MovingFile
+namespace NSModel
 {
-    private string? _fileSource;
-    private string? _fileDestination;
-    private int _fileSize;
-    private DateTime _time;
-
-    //Getter _fileSource
-    public string Get_fileSource()
+    public class M_MovingFile
     {
-        return _fileSource;
+        private string? _fileSource;
+        private string? _fileDestination;
+        private int _fileSize;
+        private DateTime _time;
+
+        //Getter _fileSource
+        public string Get_fileSource()
+        {
+            return _fileSource;
+        }
+
+        //Setter _fileSource
+        public void Set_fileSource(string value)
+        {
+            _fileSource = value;
+        }
+
+        //Getter _fileDestination
+        public string Get_fileDestination()
+        {
+            return _fileDestination;
+        }
+
+        //Setter _fileDestination
+        public void Set_fileDestination(string value)
+        {
+            _fileDestination = value;
+        }
+
+        //Getter _fileSize
+        public int Get_fileSize()
+        {
+            return _fileSize;
+        }
+
+        //Setter _fileSize
+        public void Set_fileSize(int value)
+        {
+            _fileSize = value;
+        }
+
+        //Getter _time
+        public DateTime Get_time()
+        {
+            return _time;
+        }
+
+        //Setter _time
+        public void Set_time(DateTime value)
+        {
+            _time = value;
+        }
     }
 
-    //Setter _fileSource
-    public void Set_fileSource(string value) 
-    {
-        _fileSource = value;     
-    }
-
-    //Getter _fileDestination
-    public string Get_fileDestination()
-    { 
-        return _fileDestination;     
-    }
-
-    //Setter _fileDestination
-    public void Set_fileDestination(string value)
-    {
-        _fileDestination = value;     
-    }
-
-    //Getter _fileSize
-    public int Get_fileSize()
-    {
-        return _fileSize;
-    }
-
-    //Setter _fileSize
-    public void Set_fileSize(int value)
-    {
-        _fileSize = value;
-    }
-    
-    //Getter _time
-    public DateTime Get_time()
-    {
-        return _time;
-    }
-
-    //Setter _time
-    public void Set_time(DateTime value)
-    {
-        _time = value;
-    }
 }
-
--- Livrable1/Model/SaveJob.cs
+++ Livrable1/Model/SaveJob.cs
@@ -2,163 +2,168 @@
 //Description : This class is used to store information and save the files
 
 using System.Collections.Generic;
-public class M_SaveJob
+using System;
+
+namespace NSModel
 {
-    private string _saveJobName;
-    private string _saveJobSourceDirectory;
-    private string _saveJobDestinationDirectory;
-    private string _saveJobType;
-    private M_MovingFile _file;
-    private string _state;
-    private int _totalNbFile;
-    private int _totalSizeFile;
-
-    //Getter and Setter
-
-    //Getter _savedJobName
-    public string Get_saveJobName()
+    public class M_SaveJob
     {
-        return _saveJobName;
-    }
+        private string _saveJobName;
+        private string _saveJobSourceDirectory;
+        private string _saveJobDestinationDirectory;
+        private string _saveJobType;
+        private M_MovingFile _file;
+        private string _state;
+        private int _totalNbFile;
+        private int _totalSizeFile;
 
-    //Setter _savedJobName
-    public void Set_saveJobName(string value)
-    {
-        _saveJobName = value;
-    }
+        //Getter and Setter
 
-    //Getter _saveJobSourceDirectory
-    public string Get_saveJobSourceDirectory()
-    {
-        return _saveJobSourceDirectory;
-    }
+        //Getter _savedJobName
+        public string Get_saveJobName()
+        {
+            return _saveJobName;
+        }
 
-    //Setter _saveJobSourceDirectory
-    public void Set_saveJobSourceDirectory(string value)
-    {
-        _saveJobSourceDirectory = value;
-    }
+        //Setter _savedJobName
+        public void Set_saveJobName(string value)
+        {
+            _saveJobName = value;
+        }
 
-    //Getter _saveJobDestinationDirectory
-    public string Get_saveJobDestinationDirectory()
-    {
-        return _saveJobDestinationDirectory;
-    }
+        //Getter _saveJobSourceDirectory
+        public string Get_saveJobSourceDirectory()
+        {
+            return _saveJobSourceDirectory;
+        }
 
-    //Setter _saveJobDestinationDirectory
-    public void Set_saveJobDestinationDirectory(string value)
-    {
-        _saveJobDestinationDirectory = value;
-    }
+        //Setter _saveJobSourceDirectory
+        public void Set_saveJobSourceDirectory(string value)
+        {
+            _saveJobSourceDirectory = value;
+        }
 
-    //Getter _saveJobType
-    public string Get_saveJobType()
-    {
-        return _saveJobType;
-    }
+        //Getter _saveJobDestinationDirectory
+        public string Get_saveJobDestinationDirectory()
+        {
+            return _saveJobDestinationDirectory;
+        }
 
-    //Setter _saveJobType
-    public void Set_saveJobType(string value)
-    {
-        _saveJobType = value;
-    }
+        //Setter _saveJobDestinationDirectory
+        public void Set_saveJobDestinationDirectory(string value)
+        {
+            _saveJobDestinationDirectory = value;
+        }
 
-    //Getter _file
-    public M_MovingFile Get_file()
-    {
-        return _file;
-    }
+        //Getter _saveJobType
+        public string Get_saveJobType()
+        {
+            return _saveJobType;
+        }
 
-    //Setter _file
-    public void Set_file(M_MovingFile value)
-    {
-        _file = value;
-    }
+        //Setter _saveJobType
+        public void Set_saveJobType(string value)
+        {
+            _saveJobType = value;
+        }
 
-    //Getter _state
-    public string Get_state()
-    {
-        return _state;
-    }
+        //Getter _file
+        public M_MovingFile Get_file()
+        {
+            return _file;
+        }
 
-    //Setter _state
-    public void Set_state(string value)
-    {
-        _state = value;
-    }
+        //Setter _file
+        public void Set_file(M_MovingFile value)
+        {
+            _file = value;
+        }
 
-    //Getter _totalNbFile
-    public int Get_totalNbFile()
-    {
-        return _totalNbFile;
-    }
+        //Getter _state
+        public string Get_state()
+        {
+            return _state;
+        }
 
-    //Setter _totalNbFile
-    public void Set_totalNbFile(int value)
-    {
-        _totalNbFile = value;
-    }
+        //Setter _state
+        public void Set_state(string value)
+        {
+            _state = value;
+        }
 
-    //Getter _totalSizeFile
-    public int Get_totalSizeFile()
-    {
-        return _totalSizeFile;
-    }
+        //Getter _totalNbFile
+        public int Get_totalNbFile()
+        {
+            return _totalNbFile;
+        }
 
-    //Setter _totalSizeFile
-    public void Set_totalSizeFile(int value)
-    {
-        _totalSizeFile = value;
-    }
+        //Setter _totalNbFile
+        public void Set_totalNbFile(int value)
+        {
+            _totalNbFile = value;
+        }
 
-    //Constructor (Set all attributes when object instantiation)
-    public M_SaveJob(string _saveJobName, string _saveJobSourceDirectory, string _saveJobDestinationDirectory, string _saveJobType, M_MovingFile _file, string _state, int _totalNbFile, int _totalSizeFile)
-    {
-        this.Set_saveJobName(_saveJobName);
-        this.Set_saveJobSourceDirectory(_saveJobSourceDirectory);
-        this.Set_saveJobDestinationDirectory(_saveJobDestinationDirectory);
-        this.Set_saveJobType(_saveJobType);
-        this.Set_file(_file);
-        this.Set_state(_state);
-        this.Set_totalNbFile(_totalNbFile);
-        this.Set_totalSizeFile(_totalSizeFile);
-    }
+        //Getter _totalSizeFile
+        public int Get_totalSizeFile()
+        {
+            return _totalSizeFile;
+        }
 
-    //Edit attributes of object M_SaveJob
-    public void Update(string _saveJobName, string _saveJobSourceDirectory, string _saveJobDestinationDirectory, string _saveJobType, M_MovingFile _file, string _state, int _totalNbFile, int _totalSizeFile)
-    {
-        //Edit attributes
-        this.Set_saveJobName(_saveJobName);
-        this.Set_saveJobSourceDirectory(_saveJobSourceDirectory);
-        this.Set_saveJobDestinationDirectory(_saveJobDestinationDirectory);
-        this.Set_saveJobType(_saveJobType);
-        this.Set_file(_file);
-        this.Set_state(_state);
-        this.Set_totalNbFile(_totalNbFile);
-        this.Set_totalSizeFile(_totalSizeFile);
-    }
+        //Setter _totalSizeFile
+        public void Set_totalSizeFile(int value)
+        {
+            _totalSizeFile = value;
+        }
 
-    //Get all attributes of object M_SaveJob
-    public Dictionary<string, dynamic> Read()
-    {
-        //Create a dictionary to store all attributes of object M_SaveJob
-        Dictionary<string, dynamic> values = new Dictionary<string, dynamic>();
-        //Add all attributes of object M_SaveJob to dictionary
-        values.Add("_saveJobName", this.Get_saveJobName());
-        values.Add("_saveJobSourceDirectory", this.Get_saveJobSourceDirectory());
-        values.Add("_saveJobDestinationDirectory", this.Get_saveJobDestinationDirectory());
-        values.Add("_saveJobType", this.Get_saveJobType());
-        values.Add("_file", this.Get_file());
-        values.Add("_state", this.Get_state());
-        values.Add("_totalNbFile", this.Get_totalNbFile());
-        values.Add("_totalSizeFile", this.Get_totalSizeFile());
-        //Return dictionary that contains all attributes of object M_SaveJob
-        return values;
-    }
+        //Constructor (Set all attributes when object instantiation)
+        public M_SaveJob(string _saveJobName, string _saveJobSourceDirectory, string _saveJobDestinationDirectory, string _saveJobType, M_MovingFile _file, string _state, int _totalNbFile, int _totalSizeFile)
+        {
+            this.Set_saveJobName(_saveJobName);
+            this.Set_saveJobSourceDirectory(_saveJobSourceDirectory);
+            this.Set_saveJobDestinationDirectory(_saveJobDestinationDirectory);
+            this.Set_saveJobType(_saveJobType);
+            this.Set_file(_file);
+            this.Set_state(_state);
+            this.Set_totalNbFile(_totalNbFile);
+            this.Set_totalSizeFile(_totalSizeFile);
+        }
 
-    ~M_SaveJob()
-    {
-        //Destructor
-    }
+        //Edit attributes of object M_SaveJob
+        public void Update(string _saveJobName, string _saveJobSourceDirectory, string _saveJobDestinationDirectory, string _saveJobType, M_MovingFile _file, string _state, int _totalNbFile, int _totalSizeFile)
+        {
+            //Edit attributes
+            this.Set_saveJobName(_saveJobName);
+            this.Set_saveJobSourceDirectory(_saveJobSourceDirectory);
+            this.Set_saveJobDestinationDirectory(_saveJobDestinationDirectory);
+            this.Set_saveJobType(_saveJobType);
+            this.Set_file(_file);
+            this.Set_state(_state);
+            this.Set_totalNbFile(_totalNbFile);
+            this.Set_totalSizeFile(_totalSizeFile);
+        }
 
+        //Get all attributes of object M_SaveJob
+        public Dictionary<string, dynamic> Read()
+        {
+            //Create a dictionary to store all attributes of object M_SaveJob
+            Dictionary<string, dynamic> values = new Dictionary<string, dynamic>();
+            //Add all attributes of object M_SaveJob to dictionary
+            values.Add("_saveJobName", this.Get_saveJobName());
+            values.Add("_saveJobSourceDirectory", this.Get_saveJobSourceDirectory());
+            values.Add("_saveJobDestinationDirectory", this.Get_saveJobDestinationDirectory());
+            values.Add("_saveJobType", this.Get_saveJobType());
+            values.Add("_file", this.Get_file());
+            values.Add("_state", this.Get_state());
+            values.Add("_totalNbFile", this.Get_totalNbFile());
+            values.Add("_totalSizeFile", this.Get_totalSizeFile());
+            //Return dictionary that contains all attributes of object M_SaveJob
+            return values;
+        }
+
+        ~M_SaveJob()
+        {
+            //Destructor
+        }
+
+    }
 }
