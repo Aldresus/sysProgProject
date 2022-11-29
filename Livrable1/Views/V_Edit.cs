c4d78d6f097ac1a1e860e18f08839a14f7f554ac
Hugo CHAMPY <hugo.champy@viacesi.fr>
2022-11-30 00:54:00 +0100

    modification effective

--- Livrable1/Views/V_Edit.cs
+++ Livrable1/Views/V_Edit.cs
@@ -21,14 +21,6 @@ namespace NSViews
             U_Show Show = new U_Show();
             U_Checker Checker = new U_Checker();
 
-            //TODO move code bellow to MODEL to become new language or something
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourceName = "Livrable1.Locales.locales.json";
-            Stream stream = assembly.GetManifestResourceStream(resourceName);
-            StreamReader reader = new StreamReader(stream);
-            dynamic d = JObject.Parse(reader.ReadToEnd());
-            string locale = M.Get_language();
-
             if (Checker.CheckAnyJobs(M.Get_listSaveJob()) > 0)//TODO check if there is jobs
             {
 
@@ -37,34 +29,41 @@ namespace NSViews
                 while (!validInput)
                 {
                     //TODO print all jobs currently available
-                    Console.WriteLine(d[locale].availableJobs.ToString());
+                    Console.WriteLine(M.Get_language().availableJobs.ToString());
                     Show.ShowJobs(M.Get_listSaveJob());
-                    int job = Reader.ReadInt(d[locale].whichJobToEdit.ToString());
+                    int i = Reader.ReadInt(M.Get_language().whichJobToEdit.ToString());
 
                     //check if job exists
-                    if (job >= 1 && job <= 5)//TODO change 5 by the total of jobs
+                    if (i >= 1 && i <= 5)//TODO change 5 by the total of jobs
                     {
-                        string name = Reader.ReadString(d[locale].enterJobName.ToString(), false);
+                        string name = Reader.ReadString(M.Get_language().enterJobName.ToString(), false);
                         //TODO print types
                         Console.WriteLine("1 - type 1"); //placeholder
                         Console.WriteLine("2 - type 2"); //placeholder
-                        int type = Reader.ReadInt(d[locale].enterJobType.ToString());
-                        string source = Reader.ReadString(d[locale].enterJobSource.ToString(), false);
-                        string dest = Reader.ReadString(d[locale].enterJobDestination.ToString(), false);
-                        Console.WriteLine(@$"name: {name} source:{source} dest:{dest} type:{type}"); //placeholder
+                        int type = Reader.ReadInt(M.Get_language().enterJobType.ToString());
+                        string source = Reader.ReadString(M.Get_language().enterJobSource.ToString(), false);
+                        string dest = Reader.ReadString(M.Get_language().enterJobDestination.ToString(), false);
+                        Console.WriteLine($"{M.Get_language().jobCreated.ToString()} {M.Get_language().name.ToString()}: {name} {M.Get_language().sourceFolder.ToString()}:{source} {M.Get_language().destinationFolder.ToString()}:{dest} {M.Get_language().type.ToString()}:{type}");
                         validInput = true;
-                        Reader.PressAnyKeyToContinue(d[locale].pressAnyToContinue.ToString());
+
+                        int jobIndex = i-1;
+                        M.Get_listSaveJob()[jobIndex].Set_saveJobName(name);
+                        M.Get_listSaveJob()[jobIndex].Set_saveJobSourceDirectory(source);
+                        M.Get_listSaveJob()[jobIndex].Set_saveJobDestinationDirectory(dest);
+                        M.Get_listSaveJob()[jobIndex].Set_saveJobType(type);
+                        M.WriteJSON(jobIndex);
+                        Reader.PressAnyKeyToContinue(M.Get_language().pressAnyToContinue.ToString());
                     }
                     else
                     {
-                        Console.WriteLine(d[locale].indexOutOfRange.ToString());
+                        Console.WriteLine(M.Get_language().indexOutOfRange.ToString());
                     }
                 }
             }
             else
             {
-                Console.WriteLine(d[locale].noJob.ToString());
-                Reader.PressAnyKeyToContinue(d[locale].pressAnyToContinue.ToString());
+                Console.WriteLine(M.Get_language().noJob.ToString());
+                Reader.PressAnyKeyToContinue(M.Get_language().pressAnyToContinue.ToString());
             }
         }
     }
