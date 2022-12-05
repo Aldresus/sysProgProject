664a1a00d55d49ee989f6ed2ca2504ec44ee61e0
Tom SEITZ <tom.seitz@viacesi.fr>
2022-12-05 19:20:33 +0100

    code cryptage xor

--- Cryptosoft/Program.cs
+++ Cryptosoft/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace CryptoSoft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            void EncryptFile(string inputFile, string outputFile)
+            {
+                using (var fin = new FileStream(inputFile, FileMode.Open))
+                using (var fout = new FileStream(outputFile, FileMode.Create))
+                {
+                    byte[] buffer = new byte[4096];
+                    while (true)
+                    {
+                        int bytesRead = fin.Read(buffer);
+                        if (bytesRead == 0)
+                            break;
+                        EncryptBytes(buffer, bytesRead);
+                        fout.Write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+
+            const byte Secret = 183;
+
+            void EncryptBytes(byte[] buffer, int count)
+            {
+
+                for (int i = 0; i < count; i++)
+                    buffer[i] = (byte)(buffer[i] ^ Secret);
+            }
+
+            EncryptFile(@"C:\\Users\\tomst\\OneDrive\\Bureau\\cesi_CMJN_blanc.png", @"C:\\Users\\tomst\\OneDrive\\Bureau\\cesi_CMJN_blanc2.png");
+            //EncryptFile(@"C:\\Users\\tomst\\OneDrive\\Bureau\\cesi_CMJN_blanc2.png", @"C:\\Users\\tomst\\OneDrive\\Bureau\\cesi_CMJN_blanc3.png");
+
+        }
+    }
+}
