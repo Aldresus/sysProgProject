84df52eceb6792fbfe61eabba07dd1ab152fcdd2
Hugo CHAMPY <hugo.champy@viacesi.fr>
2022-12-06 21:29:51 +0100

    projet vide

---
+++ Livrable2/Livrable2/Model/M_Model.cs
@@ -0,0 +1,223 @@
+//Class Model
+//Description : This class is used to write log file and to move files about different save.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace NSModel {
+    public class M_Model
+    {
+        private List<M_SaveJob> _listSaveJob = new List<M_SaveJob>();
+        private string _logFile;
+        private string _workFile;
+        private dynamic _language;
+
+        //Constructor
+        public M_Model()
+        {
+            string pathDirectoryLog = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments).ToString() + @"\EasySave\Log";
+            if (!Directory.Exists(pathDirectoryLog))
+            {
+                Directory.CreateDirectory(pathDirectoryLog);
+            }
+
+            string logFileName = @"\log" + DateTime.Now.ToString("ddMMyyyy") + ".json";
+            string logXmlFileName = pathDirectoryLog + logFileName.Replace(".json", ".xml");
+            
+            string pathLog = pathDirectoryLog + logFileName;
+
+            if (!File.Exists(logXmlFileName))
+            {
+                using (XmlWriter X = XmlWriter.Create(logXmlFileName, new XmlWriterSettings { Indent = true }))
+                {
+                    X.WriteStartElement("logs");
+                    X.WriteEndElement();
+                    X.Flush();
+                }
+            }
+
+            this.Set_logFile(pathLog);
+            if (!File.Exists(this.Get_logFile()))
+            {
+                string initLogFile = "{\n\t\"logs\": []\n}";
+                File.WriteAllText(this.Get_logFile(), initLogFile);
+            }
+           
+            string pathDirectoryState = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments).ToString() + @"\EasySave";
+            string pathState = pathDirectoryState + @"\State.json";
+            this.Set_workFile(pathState);
+            if (!File.Exists(this.Get_workFile()))
+            {
+                StringBuilder sb = new StringBuilder();
+                StringWriter sw = new StringWriter(sb);
+                JsonWriter writer = new JsonTextWriter(sw);
+                writer.Formatting = Newtonsoft.Json.Formatting.Indented;
+                writer.WriteStartObject();
+                writer.WritePropertyName("Name");
+                writer.WriteValue("");
+                writer.WritePropertyName("SourceFilePath");
+                writer.WriteValue("");
+                writer.WritePropertyName("TargetFilePath");
+                writer.WriteValue("");
+                writer.WritePropertyName("State");
+                writer.WriteValue("");
+                writer.WritePropertyName("Type");
+                writer.WriteValue(0);
+                writer.WritePropertyName("TotalFilesToCopy");
+                writer.WriteValue(0);
+                writer.WritePropertyName("TotalFilesSize");
+                writer.WriteValue(0);
+                writer.WritePropertyName("NbFilesLeftToDo");
+                writer.WriteValue(0);
+                writer.WritePropertyName("Progression");
+                writer.WriteValue(0);
+                writer.WriteEndObject();
+
+                //Create start State Json file
+                string startJson = "{\n\"lang\": \"en\",\n\"State\": [\n";
+
+                //Convert object JObject to string
+                string json = sb.ToString();
+
+                string file = json;
+                for (int i = 0; i < 4; i++)
+                {
+                    file += ",\n" + json;
+                }
+
+                //Create end State Json file
+                string endJson = "\n]\n}";
+
+                //Write json string to JSON file
+                File.WriteAllText(this.Get_workFile(), startJson + file + endJson);
+
+            }
+
+            JObject objJSON = JObject.Parse(File.ReadAllText(this.Get_workFile()));
+
+            int identationIndex = 0;
+            foreach (JObject i in objJSON["State"])
+            {
+                this._listSaveJob.Add(new M_SaveJob(i["Name"].ToString(), i["SourceFilePath"].ToString(), i["TargetFilePath"].ToString(), i["Type"].Value<int>(), i["State"].ToString(), i["TotalFilesToCopy"].Value<int>(), i["TotalFilesSize"].Value<int>(), identationIndex));
+                identationIndex += 1;
+            }
+
+            //Parse language
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "Livrable1.Locales.locales.json";
+            Stream stream = assembly.GetManifestResourceStream(resourceName);
+            StreamReader reader = new StreamReader(stream);
+             this._language = JObject.Parse(reader.ReadToEnd())[objJSON["lang"].ToString()];
+        }
+        
+        public void WriteLanguage(string language) {
+            JObject objJSON = JObject.Parse(File.ReadAllText(this.Get_workFile()));
+            objJSON["lang"] = language;
+
+            //Convert object JObject to string
+            string json = objJSON.ToString();
+
+            //Write json string to JSON file
+            File.WriteAllText(this.Get_workFile(), json);
+        }
+
+        //Getter and Setter
+
+        //Getter _listSaveJob
+        public List<M_SaveJob> Get_listSaveJob()
+        {
+            return this._listSaveJob;
+        }
+
+        //Getter selected SaveJob
+        public M_SaveJob GetSelectedSaveJob(int value)
+        {
+            return this._listSaveJob[value];
+        }
+
+        //Setter _listSaveJob
+        public void Set_ListSaveJob(List<M_SaveJob> values)
+        {
+            this._listSaveJob = values;
+        }
+
+        //Setter selected SaveJob
+        public void SetSelectedSaveJob(int value, M_SaveJob saveJob)
+        {
+            try
+            {
+                this._listSaveJob[value] = saveJob;
+            }
+            catch (Exception e)
+            {
+                this.Get_listSaveJob().Add(saveJob);
+            }
+}
+
+        //Getter _logFile
+        public string Get_logFile()
+        {
+            return this._logFile;
+        }
+
+        //Setter _logFile
+        public void Set_logFile(string value)
+        {
+            this._logFile = value;
+        }
+
+        //Getter _workFile
+        public string Get_workFile()
+        {
+            return this._workFile;
+        }
+
+        //Setter _workFile
+        public void Set_workFile(string value)
+        {
+            this._workFile = value;
+        }
+
+        //Getter _language
+        public dynamic Get_language()
+        {
+            return this._language;
+        }
+
+        //Setter _language
+        public void Set_language(dynamic value)
+        {
+            this._language = value;
+        }
+
+        public void InstanceNewSaveJob(string _saveJobName, string _saveJobSourceDirectory, string _saveJobDestinationDirectory, int _saveJobType, string _state, int index)
+        {
+            this._listSaveJob.Add(new M_SaveJob(_saveJobName, _saveJobSourceDirectory, _saveJobDestinationDirectory, _saveJobType, _state, index));
+        }
+
+        public void RemoveSaveJob(int index)
+        {
+            List<M_SaveJob> oldListSaveJob = this.Get_listSaveJob();
+            oldListSaveJob.RemoveAt(index);
+            this._listSaveJob = oldListSaveJob;
+
+            JObject objJSON = JObject.Parse(File.ReadAllText(this.Get_workFile()));
+
+            JArray arrayStates = (JArray)objJSON["State"];
+            
+            arrayStates.RemoveAt(index);
+
+            //Convert object JObject to string
+            string finalState = objJSON.ToString();
+
+            //Write json string to JSON file
+            File.WriteAllText(this.Get_workFile(), finalState);
+        }
+    }
+}
---
+++ Livrable2/Livrable2/Utils/U_Reader.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections.Generic;
+using NSModel;
+namespace NSUtils
+{
+    public class U_Reader
+    {
+        private M_Model _oModel;
+
+        public U_Reader(M_Model M)
+        {
+            this._oModel = M;
+        }
+
+        public int ReadInt(string promptText)
+        {
+
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                Console.WriteLine(promptText);
+                string userInput = Console.ReadLine();
+
+                if (userInput.Length > 0)
+                {
+                    try
+                    {
+                        int output = int.Parse(userInput);
+                        validInput = true;
+                        return output;
+                    }
+                    catch (System.FormatException e)
+                    {
+                        Console.WriteLine(_oModel.Get_language().onlyNumbers.ToString());
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine(_oModel.Get_language().cannotBeEmpty.ToString());
+                }
+            }
+            return -1; // should never happen
+        }
+        public string ReadString(string promptText, bool canBeEmpty)
+        {
+
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                Console.WriteLine(promptText);
+                string userInput = Console.ReadLine();
+
+                if (userInput.Length == 0 && canBeEmpty)
+                {
+                    validInput = true;
+                    return userInput;
+                }
+                else if (userInput.Length > 0)
+                {
+                    validInput = true;
+                    return userInput;
+                }
+                else
+                {
+                    Console.WriteLine(_oModel.Get_language().cannotBeEmpty.ToString());
+                }
+            }
+            return null; // should never happen
+        }
+        public List<int> ReadMany(string promptText)
+        {
+
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                Console.WriteLine(promptText);
+                string userInput = Console.ReadLine();
+
+                if (userInput.Length > 0)
+                {
+
+                    string[] temp = userInput.Replace(" ", "").Split(",");
+                    List<int> output = new List<int>();
+                    try
+                    {
+                        foreach (string i in temp)
+                        {
+                            int j = int.Parse(i);
+                            if (j >= 0 && j <= _oModel.Get_listSaveJob().Count())
+                            {
+                                output.Add(j);
+                            }
+                            else
+                            {
+                                Console.WriteLine(_oModel.Get_language().indexOutOfRange.ToString());
+                                output.Clear();
+                                break;
+                            }
+                        }
+                        if (output.Any())
+                        {
+                            validInput = true;
+                            return output;
+                        }
+                    }
+                    catch (System.FormatException e)
+                    {
+                        Console.WriteLine(_oModel.Get_language().onlyNumbers.ToString());
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine(_oModel.Get_language().cannotBeEmpty.ToString());
+                }
+            }
+            return null; // should never happen
+        }
+
+
+        public void PressAnyKeyToContinue(string text)
+        {
+            Console.WriteLine($"\n\n{text}");
+            Console.ReadKey();
+        }
+
+        public string ReadPath(string promptText, bool isDest)
+        {
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                string path = this.ReadString(promptText, false);
+                if (path[^1] is not '\\' or not '/')
+                {
+                    path += @"\";
+                }
+                if (File.Exists(path) || Directory.Exists(path) || isDest)
+                {
+                    validInput = true;
+                    return path.Replace(@"/", @"\");
+                }
+                else
+                {
+                    Console.WriteLine(_oModel.Get_language().sourcePathDoentExist.ToString());
+                }
+            }
+            return null; // should never happen
+        }
+    }
+}
